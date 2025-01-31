d6c49a5d95bab47454cbb1c24f6477f41302e782
Devanshu Acharya <darkknightcode777@gmail.com>
2025-01-31 16:52:41 +0530

    Styling commit.

--- main.js
+++ main.js
@@ -6,7 +6,17 @@ function addTask() {
     if (task) {
         task_list.innerHTML += `<li>${task}</li>`;
         task_text.value = '';
+        textarea.style.height = "auto"; 
+    }
+    else {
+        alert('Please enter a task');
     }
 }
 document.getElementById('add').onclick = addTask;
 
+const textarea = document.getElementById("task_text");
+
+textarea.addEventListener("input", function () {
+    this.style.height = "auto"; // Reset height to recalculate
+    this.style.height = this.scrollHeight + "px"; // Adjust height
+});
